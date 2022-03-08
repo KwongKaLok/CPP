61d192774180380f450e912706f784c591ce051d
Kwong Ka Lok <klkwongab@connect.ust.hk>
2022-03-09 01:08:34 +0800

    add PA_1

---
+++ Comp_2012/PA_1/polynomial.cpp
@@ -0,0 +1,5 @@
+#include "polynomial.h"
+#include <iostream>
+using namespace std;
+//note: you are not allowed to include any additional library, see also FAQ for what you can/cannot do
+//write your code below and submit this file only
