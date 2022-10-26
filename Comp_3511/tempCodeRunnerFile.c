49591c9dd590b40c495f16e97b01120555bb95a1
Kwong Ka Lok <klkwongab@connect.ust.hk>
2022-10-26 18:00:10 +0800

    added PA2

--- Comp_3511/tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
-read_tokens(arguments, pipe_segments[i], &num_arguments, SPACE_CHARS);
-        // for (j = 0; j < num_arguments; j++)
-        // {
-        //     printf("\t%d : %s\n", j, arguments[j]);
-        // }
--- Comp_3511/test.c
+++ Comp_3511/test.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+
 int main()
 {
+    char msg[50] = "empty message";
+    const char secret_msg[] = "secret message";
+    int secret_length = strlen(secret_msg);
+    int pfds[2];
+
+    pipe(pfds); // Hint: invoke pipe() here
     pid_t pid = fork();
     if (pid > 0)
-    {
-        printf("I'm parent \n");
+    {            // parent
+        wait(0); // wait for the child
 
-        // wait(0);
+        close(pfds[1]); // Hint: invoke close() here
+
+        read(pfds[0], msg, secret_length); // Hint: invoke read here
+        printf("The message in parent is %s\n", msg);
     }
     else
-    {
-        sleep(1);
-        printf("I'm children \n");
+    { // child
+        strcpy(msg, secret_msg);
+
+        close(pfds[0]); // Hint: invoke close() here
+
+        write(pfds[1], msg, secret_length); // Hint: invoke write here
+        printf("The message in child is %s\n", msg);
     }
     return 0;
 }
