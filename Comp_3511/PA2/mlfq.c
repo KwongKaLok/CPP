bbe7f8507ae6f29b6ed127aeb8385759f8abcedd
Kwong Ka Lok <klkwongab@connect.ust.hk>
2022-11-04 16:34:22 +0800

    local PA2 completed

--- Comp_3511/PA2/mlfq.c
+++ Comp_3511/PA2/mlfq.c
@@ -1,14 +1,14 @@
 /*
-    COMP3511 Fall 2022 
+    COMP3511 Fall 2022
     PA2: Simplified Multi-Level Feedback Queue (MLFQ)
 
     Your name:                 Kwong Ka Lok
-    Your ITSC email:           klkwongab@connect.ust.hk 
+    Your ITSC email:           klkwongab@connect.ust.hk
 
     Declaration:
 
     I declare that I am not involved in plagiarism
-    I understand that both parties (i.e., students providing the codes and students copying the codes) will receive 0 marks. 
+    I understand that both parties (i.e., students providing the codes and students copying the codes) will receive 0 marks.
 
 */
 
@@ -33,7 +33,7 @@
 #define KEYWORD_PROCESS_TABLE_SIZE "process_table_size"
 #define KEYWORD_PROCESS_TABLE "process_table"
 
-// Assume that we only need to support 2 types of space characters: 
+// Assume that we only need to support 2 types of space characters:
 // " " (space), "\t" (tab)
 #define SPACE_CHARS " \t"
 
@@ -41,22 +41,26 @@
 // Helper functions:
 //  process_init: initialize a process entry
 //  process_table_print: Display the process table
-struct Process {
+struct Process
+{
     char name[MAX_PROCESS_NAME];
-    int arrival_time ;
+    int arrival_time;
     int burst_time;
-    int remain_time; // remain_time is needed in the intermediate steps of MLFQ 
+    int remain_time; // remain_time is needed in the intermediate steps of MLFQ
 };
-void process_init(struct Process* p, char name[MAX_PROCESS_NAME], int arrival_time, int burst_time) {
+void process_init(struct Process *p, char name[MAX_PROCESS_NAME], int arrival_time, int burst_time)
+{
     strcpy(p->name, name);
     p->arrival_time = arrival_time;
     p->burst_time = burst_time;
     p->remain_time = 0;
 }
-void process_table_print(struct Process* p, int size) {
+void process_table_print(struct Process *p, int size)
+{
     int i;
     printf("Process\tArrival\tBurst\n");
-    for (i=0; i<size; i++) {
+    for (i = 0; i < size; i++)
+    {
         printf("%s\t%d\t%d\n", p[i].name, p[i].arrival_time, p[i].burst_time);
     }
 }
@@ -70,46 +74,56 @@ void process_table_print(struct Process* p, int size) {
 //   queue_enqueue: insert one item at the end of the queue
 //   queue_dequeue: remove one item from the beginning of the queue
 //   queue_print: display the queue content, it is useful for debugging
-struct Queue {
+struct Queue
+{
     int values[MAX_QUEUE_SIZE];
     int front, rear, count;
 };
-void queue_init(struct Queue* q) {
+void queue_init(struct Queue *q)
+{
     q->count = 0;
     q->front = 0;
     q->rear = -1;
 }
-int queue_is_empty(struct Queue* q) {
+int queue_is_empty(struct Queue *q)
+{
     return q->count == 0;
 }
-int queue_is_full(struct Queue* q) {
+int queue_is_full(struct Queue *q)
+{
     return q->count == MAX_QUEUE_SIZE;
 }
 
-int queue_peek(struct Queue* q) {
+int queue_peek(struct Queue *q)
+{
     return q->values[q->front];
 }
-void queue_enqueue(struct Queue* q, int new_value) {
-    if (!queue_is_full(q)) {
-        if ( q->rear == MAX_QUEUE_SIZE -1)
+void queue_enqueue(struct Queue *q, int new_value)
+{
+    if (!queue_is_full(q))
+    {
+        if (q->rear == MAX_QUEUE_SIZE - 1)
             q->rear = -1;
         q->values[++q->rear] = new_value;
         q->count++;
     }
 }
-void queue_dequeue(struct Queue* q) {
+void queue_dequeue(struct Queue *q)
+{
     q->front++;
     if (q->front == MAX_QUEUE_SIZE)
         q->front = 0;
     q->count--;
 }
-void queue_print(struct Queue* q) {
+void queue_print(struct Queue *q)
+{
     int c = q->count;
     printf("size = %d\n", c);
     int cur = q->front;
     printf("values = ");
-    while ( c > 0 ) {
-        if ( cur == MAX_QUEUE_SIZE )
+    while (c > 0)
+    {
+        if (cur == MAX_QUEUE_SIZE)
             cur = 0;
         printf("%d ", q->values[cur]);
         cur++;
@@ -122,32 +136,36 @@ void queue_print(struct Queue* q) {
 // Helper functions:
 //   gantt_chart_update: append one item to the end of the chart (or update the last item if the new item is the same as the last item)
 //   gantt_chart_print: display the current chart
-struct GanttChartItem {
+struct GanttChartItem
+{
     char name[MAX_PROCESS_NAME];
     int duration;
 };
-void gantt_chart_update(struct GanttChartItem chart[MAX_GANTT_CHART], int* n, char name[MAX_PROCESS_NAME], int duration) {
+void gantt_chart_update(struct GanttChartItem chart[MAX_GANTT_CHART], int *n, char name[MAX_PROCESS_NAME], int duration)
+{
     int i;
     i = *n;
     // The new item is the same as the last item
-    if ( i > 0 && strcmp(chart[i-1].name, name) == 0) 
+    if (i > 0 && strcmp(chart[i - 1].name, name) == 0)
     {
-        chart[i-1].duration += duration; // update duration
-    } 
+        chart[i - 1].duration += duration; // update duration
+    }
     else
     {
         strcpy(chart[i].name, name);
         chart[i].duration = duration;
-        *n = i+1;
+        *n = i + 1;
     }
 }
-void gantt_chart_print(struct GanttChartItem chart[MAX_GANTT_CHART], int n) {
+void gantt_chart_print(struct GanttChartItem chart[MAX_GANTT_CHART], int n)
+{
     int t = 0;
     int i = 0;
     printf("Gantt Chart = ");
     printf("%d ", t);
-    for (i=0; i<n; i++) {
-        t = t + chart[i].duration;     
+    for (i = 0; i < n; i++)
+    {
+        t = t + chart[i].duration;
         printf("%s %d ", chart[i].name, t);
     }
     printf("\n");
@@ -158,32 +176,35 @@ int tq0 = 0, tq1 = 0;
 int process_table_size = 0;
 struct Process process_table[MAX_NUM_PROCESS];
 
-
-
 // Helper function: Check whether the line is a blank line (for input parsing)
-int is_blank(char *line) {
+int is_blank(char *line)
+{
     char *ch = line;
-    while ( *ch != '\0' ) {
-        if ( !isspace(*ch) )
+    while (*ch != '\0')
+    {
+        if (!isspace(*ch))
             return 0;
         ch++;
     }
     return 1;
 }
 // Helper function: Check whether the input line should be skipped
-int is_skip(char *line) {
-    if ( is_blank(line) )
+int is_skip(char *line)
+{
+    if (is_blank(line))
         return 1;
-    char *ch = line ;
-    while ( *ch != '\0' ) {
-        if ( !isspace(*ch) && *ch == '#')
+    char *ch = line;
+    while (*ch != '\0')
+    {
+        if (!isspace(*ch) && *ch == '#')
             return 1;
         ch++;
     }
     return 0;
 }
 // Helper: parse_tokens function
-void parse_tokens(char **argv, char *line, int *numTokens, char *delimiter) {
+void parse_tokens(char **argv, char *line, int *numTokens, char *delimiter)
+{
     int argc = 0;
     char *token = strtok(line, delimiter);
     while (token != NULL)
@@ -195,15 +216,16 @@ void parse_tokens(char **argv, char *line, int *numTokens, char *delimiter) {
 }
 
 // Helper: parse the input file
-void parse_input() {
+void parse_input()
+{
     FILE *fp = stdin;
     char *line = NULL;
     ssize_t nread;
     size_t len = 0;
 
-    char *two_tokens[2]; // buffer for 2 tokens
+    char *two_tokens[2];   // buffer for 2 tokens
     char *three_tokens[3]; // buffer for 3 tokens
-    int numTokens = 0, n=0, i=0;
+    int numTokens = 0, n = 0, i = 0;
     char equal_plus_spaces_delimiters[5] = "";
 
     char process_name[MAX_PROCESS_NAME];
@@ -211,53 +233,61 @@ void parse_input() {
     int process_burst_time = 0;
 
     strcpy(equal_plus_spaces_delimiters, "=");
-    strcat(equal_plus_spaces_delimiters,SPACE_CHARS);
+    strcat(equal_plus_spaces_delimiters, SPACE_CHARS);
 
-    while ( (nread = getline(&line, &len, fp)) != -1 ) {
-        if ( is_skip(line) == 0)  {
-            line = strtok(line,"\n");
+    while ((nread = getline(&line, &len, fp)) != -1)
+    {
+        if (is_skip(line) == 0)
+        {
+            line = strtok(line, "\n");
 
-            if (strstr(line, KEYWORD_TQ0)) {
+            if (strstr(line, KEYWORD_TQ0))
+            {
                 // parse tq0
                 parse_tokens(two_tokens, line, &numTokens, equal_plus_spaces_delimiters);
-                if (numTokens == 2) {
+                if (numTokens == 2)
+                {
                     sscanf(two_tokens[1], "%d", &tq0);
                 }
-            } 
-            else if (strstr(line, KEYWORD_TQ1)) {
+            }
+            else if (strstr(line, KEYWORD_TQ1))
+            {
                 // parse tq0
                 parse_tokens(two_tokens, line, &numTokens, equal_plus_spaces_delimiters);
-                if (numTokens == 2) {
+                if (numTokens == 2)
+                {
                     sscanf(two_tokens[1], "%d", &tq1);
                 }
             }
-            else if (strstr(line, KEYWORD_PROCESS_TABLE_SIZE)) {
+            else if (strstr(line, KEYWORD_PROCESS_TABLE_SIZE))
+            {
                 // parse process_table_size
                 parse_tokens(two_tokens, line, &numTokens, equal_plus_spaces_delimiters);
-                if (numTokens == 2) {
+                if (numTokens == 2)
+                {
                     sscanf(two_tokens[1], "%d", &process_table_size);
                 }
-            } 
-            else if (strstr(line, KEYWORD_PROCESS_TABLE)) {
+            }
+            else if (strstr(line, KEYWORD_PROCESS_TABLE))
+            {
 
                 // parse process_table
-                for (i=0; i<process_table_size; i++) {
+                for (i = 0; i < process_table_size; i++)
+                {
 
                     getline(&line, &len, fp);
-                    line = strtok(line,"\n");  
+                    line = strtok(line, "\n");
 
                     sscanf(line, "%s %d %d", process_name, &process_arrival_time, &process_burst_time);
                     process_init(&process_table[i], process_name, process_arrival_time, process_burst_time);
-
                 }
             }
-
         }
-        
     }
 }
 // Helper: Display the parsed values
-void print_parsed_values() {
+void print_parsed_values()
+{
     printf("%s = %d\n", KEYWORD_TQ0, tq0);
     printf("%s = %d\n", KEYWORD_TQ1, tq1);
     printf("%s = \n", KEYWORD_PROCESS_TABLE);
@@ -265,21 +295,103 @@ void print_parsed_values() {
 }
 
 // TODO: Implementation of MLFQ algorithm
-void mlfq() {
+void mlfq()
+{
 
     // Initialize the gantt chart
     struct GanttChartItem chart[MAX_GANTT_CHART];
     int sz_chart = 0;
 
     // TODO: implement your MLFQ algorithm here
+    char buf[MAX_PROCESS_NAME];
+    struct Queue q0; // RR with tq0
+    struct Queue q1; // RR with tq1
+    struct Queue q2; // FCFS
+    queue_init(&q0);
+    queue_init(&q1);
+    queue_init(&q2);
+    int current_process = 0;
+    int next_process = 0;
+    int clock = 0;
+    int remain_tq0 = tq0;
+    int remain_tq1 = tq1;
+    while (clock <= MAX_GANTT_CHART) // clock
+    {
+        if (!queue_is_full(&q0) && process_table[next_process].arrival_time <= clock && next_process < process_table_size)
+        {
+            queue_enqueue(&q0, next_process);
+            process_table[next_process].remain_time = process_table[next_process].burst_time;
+            next_process++;
+        }
+        clock++;
+        if (!queue_is_empty(&q0))
+        {
+            current_process = queue_peek(&q0);
 
+            if (process_table[current_process].remain_time != 0 && remain_tq0 != 0)
+            {
+                process_table[current_process].remain_time--;
+                remain_tq0--;
+                gantt_chart_update(chart, &sz_chart, process_table[current_process].name, 1);
+            }
+            if (process_table[current_process].remain_time > 0 && remain_tq0 == 0)
+            {
+                queue_dequeue(&q0);
+                queue_enqueue(&q1, current_process);
+                remain_tq0 = tq0;
+            }
+            if (process_table[current_process].remain_time == 0 && remain_tq0 >= 0)
+            {
+                queue_dequeue(&q0);
+                remain_tq0 = tq0;
+            }
+            continue;
+        }
+
+        if (queue_is_empty(&q0) && !queue_is_empty(&q1))
+        {
+            current_process = queue_peek(&q1);
+            if (process_table[current_process].remain_time != 0 && remain_tq1 != 0)
+            {
+                process_table[current_process].remain_time--;
+                remain_tq1--;
+                gantt_chart_update(chart, &sz_chart, process_table[current_process].name, 1);
+            }
+            if (process_table[current_process].remain_time > 0 && remain_tq1 == 0)
+            {
+                queue_dequeue(&q1);
+                queue_enqueue(&q2, current_process);
+                remain_tq1 = tq1;
+            }
+            if (process_table[current_process].remain_time == 0 && remain_tq1 >= 0)
+            {
+                queue_dequeue(&q1);
+                remain_tq1 = tq1;
+            }
+            continue;
+        }
+        if (queue_is_empty(&q0) && queue_is_empty(&q1) && !queue_is_empty(&q2))
+        {
+            current_process = queue_peek(&q2);
+            if (process_table[current_process].remain_time != 0)
+            {
+                process_table[current_process].remain_time--;
+                gantt_chart_update(chart, &sz_chart, process_table[current_process].name, 1);
+            }
+            if (process_table[current_process].remain_time == 0)
+            {
+                queue_dequeue(&q2);
+            }
+            continue;
+        }
+    }
 
     // At the end, uncomment this line to display the final Gantt chart
-    // gantt_chart_print(chart, sz_chart);
+    gantt_chart_print(chart, sz_chart);
 }
 
-
-int main() {
+int main()
+{
     parse_input();
     print_parsed_values();
     mlfq();
--- Comp_3511/test.c
+++ Comp_3511/test.c
@@ -5,30 +5,13 @@
 
 int main()
 {
-    char msg[50] = "empty message";
-    const char secret_msg[] = "secret message";
-    int secret_length = strlen(secret_msg);
-    int pfds[2];
-
-    pipe(pfds); // Hint: invoke pipe() here
-    pid_t pid = fork();
-    if (pid > 0)
-    {            // parent
-        wait(0); // wait for the child
-
-        close(pfds[1]); // Hint: invoke close() here
-
-        read(pfds[0], msg, secret_length); // Hint: invoke read here
-        printf("The message in parent is %s\n", msg);
+    int pid;
+    printf("%d\n", getpid());
+    pid = fork();
+    if (pid == 0)
+    {
+        printf("%d\n", getpid());
     }
-    else
-    { // child
-        strcpy(msg, secret_msg);
 
-        close(pfds[0]); // Hint: invoke close() here
-
-        write(pfds[1], msg, secret_length); // Hint: invoke write here
-        printf("The message in child is %s\n", msg);
-    }
     return 0;
 }
